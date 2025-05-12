6672beafea4cf34303ec7cb265425cd0918545e4
Joshua Walker <walkerje1992@gmail.com>
2025-05-12 17:10:07 -0600

    add simple blinky test

---
+++ esp-stuff/blinky/main/blinky_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+void app_main() {
+    printf("Begin Blinky\n");
+
+	gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+
+    while (true) {
+        printf("LED ON\n");
+        gpio_set_level(GPIO_NUM_2, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        printf("LED OFF\n");
+        gpio_set_level(GPIO_NUM_2, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+}
