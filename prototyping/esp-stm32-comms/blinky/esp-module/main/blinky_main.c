f398ce4649e841858825176464703f2edf238258
Joshua Walker <walkerje1992@gmail.com>
2025-05-21 18:03:45 -0600

    add basic transmission

--- prototyping/esp-stm32-comms/blinky/esp-module/main/blinky_main.c
+++ prototyping/esp-stm32-comms/blinky/esp-module/main/blinky_main.c
@@ -4,6 +4,14 @@
 #include "driver/spi.h"
 #include "driver/gpio.h"
 
+#define SLAVE_CS_PIN GPIO_NUM_16
+
+void select_slave(gpio_num_t slave_cs);
+void unselect_slave(gpio_num_t slave_cs);
+esp_err_t master_transmit(uint32_t* data, uint32_t len);
+
+uint32_t on_cmd = 0x01;
+
 void app_main(void)
 {
     printf("Starting minimal init\n");
@@ -24,19 +32,65 @@ void app_main(void)
         printf("SPI init success\n");
     }
 
-    // 🛠️ Manually reconfigure GPIO15 as OUTPUT LOW to prevent reboot issue
-    gpio_config_t io_conf = {
-        .pin_bit_mask = 1ULL << 15,
+    gpio_config_t io_15_conf = {
+        .pin_bit_mask = 1 << 15,
         .mode = GPIO_MODE_OUTPUT,
         .pull_down_en = GPIO_PULLDOWN_ENABLE,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
-    gpio_config(&io_conf);
-    gpio_set_level(GPIO_NUM_15, 0);  // Force GPIO15 LOW
+    gpio_config(&io_15_conf);
+    gpio_set_level(GPIO_NUM_15, 0);  
+
+    gpio_config_t io_16_conf = {
+        .pin_bit_mask = 1 << 16,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_16_conf);
+    unselect_slave(SLAVE_CS_PIN);
 
     while (true) {
+		printf("Transmitting ON CMD....\n");
+        select_slave(SLAVE_CS_PIN);
+		esp_err_t status = master_transmit(&on_cmd, sizeof(on_cmd));
+
+        if (status != ESP_OK) {
+            printf("Transmission failed\n");
+        } else {
+            printf("Success!\n");
+        }
+        unselect_slave(SLAVE_CS_PIN);
+
+
         printf("Looping...\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+void select_slave(gpio_num_t slave_cs) {
+    gpio_set_level(slave_cs, 0);
+}
+
+void unselect_slave(gpio_num_t slave_cs) {
+    gpio_set_level(slave_cs, 1);
+}
+
+esp_err_t master_transmit(uint32_t* data, uint32_t len) {
+    spi_trans_t trans;
+    uint16_t cmd; 
+    uint32_t addr = 0;
+
+	cmd = SPI_MASTER_WRITE_DATA_TO_SLAVE_CMD;
+    trans.bits.mosi = len * 32;
+    trans.mosi = data;
+
+    trans.bits.cmd = 8;
+    trans.bits.addr = 8;
+    trans.cmd = &cmd;
+    trans.addr = &addr;
+
+    return spi_trans(HSPI_HOST, &trans);
 }
