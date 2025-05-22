2ef5490df23025404f305f3cb0bbb8f03d2a4820
Joshua Walker <walkerje1992@gmail.com>
2025-05-22 16:30:25 -0600

    complete comm loop

--- prototyping/esp-stm32-comms/blinky/esp-module/main/blinky_main.c
+++ prototyping/esp-stm32-comms/blinky/esp-module/main/blinky_main.c
@@ -3,6 +3,7 @@
 #include "freertos/task.h"
 #include "driver/spi.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 
 #define SLAVE_CS_PIN GPIO_NUM_16
 
@@ -10,11 +11,14 @@ void select_slave(gpio_num_t slave_cs);
 void unselect_slave(gpio_num_t slave_cs);
 esp_err_t master_transmit(uint32_t* data, uint32_t len);
 
-uint32_t on_cmd = 0x01;
+const uint32_t ON_CMD = 0x01;
+const uint32_t OFF_CMD = 0x02;
+
+const char *TAG = "spi_comm";
 
 void app_main(void)
 {
-    printf("Starting minimal init\n");
+    ESP_LOGI(TAG, "Starting minimal init");
 
     spi_config_t spi_config = {
         .interface.val = SPI_DEFAULT_INTERFACE,
@@ -27,9 +31,9 @@ void app_main(void)
 
     esp_err_t ret = spi_init(HSPI_HOST, &spi_config);
     if (ret != ESP_OK) {
-        printf("SPI init failed: %d\n", ret);
+        ESP_LOGE(TAG, "SPI init failed: %d", ret);
     } else {
-        printf("SPI init success\n");
+        ESP_LOGI(TAG, "SPI init success");
     }
 
     gpio_config_t io_15_conf = {
@@ -53,19 +57,31 @@ void app_main(void)
     unselect_slave(SLAVE_CS_PIN);
 
     while (true) {
-		printf("Transmitting ON CMD....\n");
+        ESP_LOGI(TAG, "Transmitting ON CMD...");
         select_slave(SLAVE_CS_PIN);
-		esp_err_t status = master_transmit(&on_cmd, sizeof(on_cmd));
+		esp_err_t status = master_transmit(&ON_CMD, sizeof(ON_CMD));
 
         if (status != ESP_OK) {
-            printf("Transmission failed\n");
+            ESP_LOGE(TAG, "ON Transmission failed");
         } else {
-            printf("Success!\n");
+            ESP_LOGI(TAG, "ON Transmitted!");
         }
         unselect_slave(SLAVE_CS_PIN);
 
+		vTaskDelay(pdMS_TO_TICKS(1000));
 
-        printf("Looping...\n");
+        ESP_LOGI(TAG, "Transmitting OFF CMD...");
+        select_slave(SLAVE_CS_PIN);
+		status = master_transmit(&OFF_CMD, sizeof(OFF_CMD));
+
+        if (status != ESP_OK) {
+            ESP_LOGE(TAG, "OFF Transmission failed");
+        } else {
+            ESP_LOGI(TAG, "OFF Transmitted");
+        }
+        unselect_slave(SLAVE_CS_PIN);
+
+        ESP_LOGI(TAG, "Looping...");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
