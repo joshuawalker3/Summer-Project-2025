e118c8e46f81f7fb906032f8844263ef3be69da1
Joshua Walker <walkerje1992@gmail.com>
2025-05-24 17:02:59 -0600

    update code for using wifi header for credentials

--- prototyping/esp-stm32-comms/wifi-led/esp-module/main/station_example_main.c
+++ prototyping/esp-stm32-comms/wifi-led/esp-module/main/station_example_main.c
@@ -21,13 +21,18 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
+#include "my_wifi.h"
+
 /* The examples use WiFi configuration that you can set via project configuration menu
 
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#ifndef MY_WIFI_
+    #define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+    #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#endif
+
 #define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
 
 /* FreeRTOS event group to signal when we are connected*/
