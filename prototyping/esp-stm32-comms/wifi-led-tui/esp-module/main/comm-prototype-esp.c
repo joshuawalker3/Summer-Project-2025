2b42c4eb7bad1f186a8268216e16145ce8c08dde
Joshua Walker <walkerje1992@gmail.com>
2025-05-28 17:38:47 -0600

    update socket task for new flow

--- prototyping/esp-stm32-comms/wifi-led-tui/esp-module/main/comm-prototype-esp.c
+++ prototyping/esp-stm32-comms/wifi-led-tui/esp-module/main/comm-prototype-esp.c
@@ -46,59 +46,86 @@ void socket_client_task(void *pvParameters)
 
     int sock = tcp_create_and_connect(HOST_IP, HOST_PORT);
 
+    if (sock < 0) {
+        vTaskDelete(NULL);
+    }
+
 	uint8_t status = 0x00;
 
     while (true) {
+        int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+
+        if (len < 0) {
+            ESP_LOGE(TAG, "recv failed: errno %d", errno);
+
+            break;
+        }
+
+        rx_buffer[len] = '\0';
+
+        ESP_LOGI(TAG, "Received: %s", rx_buffer);
+
+        cJSON* received = cJSON_Parse(rx_buffer);
+
+        uint8_t cmd = (uint8_t)(cJSON_GetObjectItem(received, "CMD")->valueint);
+
+        ESP_LOGI(TAG, "Received cmd %d", cmd);
+
+        cJSON_Delete(received);
+
         cJSON* root = cJSON_CreateObject();
 
-        if (root == NULL) {
-            ESP_LOGI(TAG, "root null");
+        esp_err_t err;
+
+        uint8_t success = 0x01;
+        char success_bit[2];
+
+        switch(cmd) {
+        case 0x00:
+            select_slave(SLAVE_CS_PIN);
+            err = master_transmit(&cmd);
+            unselect_slave(SLAVE_CS_PIN);
+
+            if (err != ESP_OK) {
+                success = 0x00;
+
+                cJSON_AddStringToObject(root, "ERROR", esp_err_to_name(err));
+
+                ESP_LOGE(TAG, "Error while processing cmd 0x00: %s", esp_err_to_name(err));
+            }
+
+            break;
+        case 0x01:
+            select_slave(SLAVE_CS_PIN);
+            err = master_transmit(&cmd);
+            unselect_slave(SLAVE_CS_PIN);
+
+            if (err != ESP_OK) {
+                success = 0x00;
+
+                cJSON_AddStringToObject(root, "ERROR", esp_err_to_name(err));
+
+                ESP_LOGE(TAG, "Error while processing cmd 0x01: %s", esp_err_to_name(err));
+            }
+
+            break;
+        case 0x02:
+            break;
+        default:
+            break;
         }
 
-        char status_send[4];
-        snprintf(status_send, sizeof(status_send), "%u", status);
+        snprintf(success_bit, sizeof(success_bit), "%u", success);
 
-    	if (!cJSON_AddStringToObject(root, "STATUS", status_send)) {
-            ESP_LOGI(TAG, "Failed to add status");
-        }
+        cJSON_AddStringToObject(root, "SUCCESS", success_bit);
 
-    	char* payload = cJSON_PrintUnformatted(root);
-
-        if (payload == NULL) {
-            ESP_LOGI(TAG, "Payload null");
-        }
+        char* payload = cJSON_PrintUnformatted(root);
 
         ESP_LOGI(TAG, "Sending %s", payload);
-    	send(sock, payload, strlen(payload), 0);
+        send(sock, payload, strlen(payload), 0);
 
         cJSON_free(payload);
         cJSON_Delete(root);
-
-    	int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-
-    	if (len < 0) {
-        	ESP_LOGE(TAG, "recv failed: errno %d", errno);
-    	} else {
-        	rx_buffer[len] = '\0';
-
-        	ESP_LOGI(TAG, "Received: %s", rx_buffer);
-
-			cJSON* received = cJSON_Parse(rx_buffer);
-
-            uint8_t cmd = (uint8_t)(cJSON_GetObjectItem(received, "CMD")->valueint);
-
-            ESP_LOGI(TAG, "Received cmd %d", cmd);
-
-            cJSON_Delete(received);
-
-            select_slave(SLAVE_CS_PIN);
-            master_transmit(&cmd);
-            unselect_slave(SLAVE_CS_PIN);
-    	}
-
-        status ^= 0x01;
-
-        vTaskDelay(1000 /  portTICK_PERIOD_MS);
     }
 
     shutdown(sock, 0);
